d3acf8262359fe4b5b902772e95379b2e82bb90d
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2016-07-25 00:41:21 +0530

    get url from user

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ extern crate env_logger;
 extern crate clap;
 use clap::App;
 
+extern crate url;
+use url::Url;
+
 use std::path::Path;
 use std::fs;
 
@@ -16,13 +19,7 @@ fn main() {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    let output_dir = Path::new({
-        if matches.is_present("outputDir") {
-            matches.value_of("outputDir").unwrap()
-        } else {
-            "./offline-website"
-        }
-    });
+    let output_dir = Path::new(matches.value_of("outputDir").unwrap_or("./offline-website"));
 
     if !output_dir.exists() {
         match fs::create_dir_all(output_dir) {
@@ -30,5 +27,16 @@ fn main() {
             Ok(_) => warn!("{} doesn't exist so creating it", output_dir.display()),
         }
     }
+
+    let parse_op = Url::parse(matches.value_of("url").unwrap());
+
+    if parse_op.is_err() {
+        error!("{:?}", parse_op.unwrap());
+        return;
+    }
+
+    let base_url_to_crawl_from = parse_op.unwrap();
+    info!("Crawler will begin downloading resources as it navigates from {}",
+          base_url_to_crawl_from);
 
 }
